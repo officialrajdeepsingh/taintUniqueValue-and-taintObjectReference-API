8c3dd63d3efb471abb4df09426e6b07246b8661d
Rajdeep Singh <radewala.2@gmail.com>
2025-02-18 19:39:07 +0530

    remove the code

--- utils/utils.ts
+++ utils/utils.ts
@@ -1,49 +1,14 @@
-import "server-only";
-import {
-  experimental_taintObjectReference as taintObjectReference,
-  experimental_taintUniqueValue as taintUniqueValue,
-} from "react";
+import { experimental_taintObjectReference as taintObjectReference } from "react";
 
 export async function getDBUser() {
-  const data = await fetch(`https://dummyjson.com/users/1`);
+  const userID = process.env.USER_ID;
+
+  const data = await fetch(`https://dummyjson.com/users/${userID}`);
   const user = await data.json();
 
-  taintObjectReference("Do not pass the whole user object to the client", user);
-
-  // taintUniqueValue(
-  //   "Do not pass the user's bank information to the client",
-  //   user,
-  //   user.bank,
-  // );
-
-  const {
-    firstName,
-    lastName,
-    maidenName,
-    age,
-    gender,
-    email,
-    phone,
-    username,
-    password,
-    birthDate,
-    company: { department, address },
-  } = user;
-
-  return {
-    user: {
-      firstName,
-      lastName,
-      maidenName,
-      age,
-      gender,
-      email,
-      phone,
-      username,
-      password,
-      birthDate,
-      department,
-      address: address.address,
-    },
-  };
+  taintObjectReference(
+    "Do not pass ALL environment variables to the client.",
+    process.env,
+  );
+  return { user };
 }
