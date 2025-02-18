5468bd0bace600bdbad3b6f513de0f5fc0850b2c
Rajdeep Singh <radewala.2@gmail.com>
2025-02-18 15:43:20 +0530

    remove the server only package

--- utils/server-only-utils.ts
+++
@@ -1,9 +0,0 @@
-import "server-only";
-
-export async function getServerOnlyUser() {
-  const SecondUserID = process.env.SECOND_USER_ID;
-  const data = await fetch(`https://dummyjson.com/users/${SecondUserID}`);
-  const user = await data.json();
-  console.log(user);
-  return { server_only_user: user };
-}
--- utils/server-utils.ts
+++
@@ -1,7 +0,0 @@
-export async function getServerUser() {
-  const userID = process.env.USER_ID;
-  const data = await fetch(`https://dummyjson.com/users/${userID}`);
-  const user = await data.json();
-  console.log(user);
-  return { server_user: user };
-}
