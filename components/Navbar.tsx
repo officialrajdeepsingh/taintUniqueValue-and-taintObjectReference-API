eaaff5e9bf9aa7b9590b9d5e5422c0d5ee939d63
Rajdeep Singh <radewala.2@gmail.com>
2025-02-18 15:43:31 +0530

    Navbar Component

---
+++ components/Navbar.tsx
@@ -0,0 +1,30 @@
+import Link from "next/link";
+
+export function Navbar() {
+  return (
+    <div className=" container flex flex-row items-center justify-between">
+      <Link
+        href="/"
+        className="text-center text-2xl bg-black text-white p-2 rounded"
+      >
+        {" "}
+        Home
+      </Link>
+      <Link
+        href="/taintUniqueValue"
+        className="text-center text-2xl bg-black text-white p-2 rounded "
+      >
+        {" "}
+        Protected Page by taintUniqueValue{" "}
+      </Link>
+
+      <Link
+        href="/taintObjectReference"
+        className="text-center text-2xl bg-black text-white p-2 rounded "
+      >
+        {" "}
+        Protected Page by taintObjectReference
+      </Link>
+    </div>
+  );
+}
